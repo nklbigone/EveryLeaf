6e252d2e16cd0295036a9e187e04d29e179eebfc
Alexis <nklbigone@gmail.com>
2019-10-01 10:31:59 +0200

    correction

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module Everyleaf
     config.i18n.default_locale = :ja
     config.time_zone = 'Cairo'
     config.active_record.default_timezone = :local
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
