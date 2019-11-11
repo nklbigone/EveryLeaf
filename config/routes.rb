047183950650f9e9dccced40f156ba9186e6dd6d
nklbigone@gmail.com <nklbigone@gmail.com>
2019-11-11 18:00:49 +0200

    step23 has been finished

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,5 @@ Rails.application.routes.draw do
     resources :users
     
   end
-  %w( 404 422 500 503 ).each do |code|
-    get code, :to => "errors#show", :code => code
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
