db11d2df7c20d0196f358bc121b3cfcd5a0ceee5
nklbigone@gmail.com <nklbigone@gmail.com>
2019-11-11 17:55:52 +0200

    step23 has been complented

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   namespace :admin do
     resources :users
+    
+  end
+  %w( 404 422 500 503 ).each do |code|
+    get code, :to => "errors#show", :code => code
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
